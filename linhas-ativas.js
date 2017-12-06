95fd61f816e7199d5311f512684250d466234b97
mauriciova <mauriciova@gmail.com>
2017-12-06 18:39:17 -0500

    pagina linhas ativas pronta

--- linhas-ativas.js
+++ linhas-ativas.js
@@ -1,13 +1,45 @@
 "use strict"
 
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML =
-            this.responseText;
-       }
-    };
-    xhttp.open("GET", "ajax_info.txt", true);
-    xhttp.send(); 
+function mostrar(elemento){
+  document.getElementById(elemento).style.display = "block";
+}
+
+function esconder(elemento){
+  document.getElementById(elemento).style.display = "none";
+}
+
+function mostrarStatusEspera(){
+  esconder("resultado");
+  mostrar("statusEspera");
+}
+
+function mostrarResultado(){
+  esconder("statusEspera");
+  mostrar("resultado");
+}
+
+function buscarLinhasAtivas() {
+  mostrarStatusEspera();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if(this.readyState == 4 && this.status == 200) {
+          apresentarLinhasAtivas(JSON.parse(this.responseText));
+     }
+  };
+  xhttp.open("GET", "back-end/public/linhas-ativas", true);
+  xhttp.send(); 
+}
+
+function apresentarLinhasAtivas(linhasAtivas){
+  var botoes = "";
+  for(var i = 0; i < linhasAtivas.length; i++) { 
+    botoes += criarBotao(linhasAtivas[i]);
+  }
+  document.getElementById("resultado").innerHTML = botoes;
+  mostrarResultado();
+}
+
+function criarBotao(linha){
+  return "<a href=\"mapa.html?linha=" + linha + 
+    "\" class=\"w3-bar-item w3-button w3-round-large w3-white w3-border\" style=\"width:10%\"><i class=\"fa fa-bus fa-2x\"></i><br><b>" + linha + "</b></a>";
 }
